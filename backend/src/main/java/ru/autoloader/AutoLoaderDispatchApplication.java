9f9fbd641d7afcbf2964e5e64fabaf6f2f5b2d4c
Salavat <Justdrumm@yandex.ru>
2025-02-28 16:17:12 +0300

    added Docker conatiners

---
+++ backend/src/main/java/ru/autoloader/AutoLoaderDispatchApplication.java
@@ -0,0 +1,13 @@
+package ru.autoloader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoLoaderDispatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoLoaderDispatchApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/ru/autoloader/AutoLoaderDispApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.autoloader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoLoaderDispApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
