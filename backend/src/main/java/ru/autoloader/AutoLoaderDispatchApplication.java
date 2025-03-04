ebf47fc2b2e1d9ba1af3f97beb35fd62e1e0da6b
Salavat <Justdrumm@yandex.ru>
2025-03-04 21:44:46 +0300

    bugfix: spting scan

--- backend/src/main/java/ru/autoloader/AutoLoaderDispatchApplication.java
+++ backend/src/main/java/ru/autoloader/AutoLoaderDispatchApplication.java
@@ -2,8 +2,14 @@ package ru.autoloader;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "ru.autoloader")  // ✅ Добавляем сканирование контроллеров
+@EnableJpaRepositories("ru.autoloader.repository")  // ✅ Добавляем репозитории
+@EntityScan("ru.autoloader.model")
 public class AutoLoaderDispatchApplication {
 
 	public static void main(String[] args) {
